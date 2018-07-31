e600bed08515ccc0988367d856b5dfba8c3ef698
SteRoa <steroagna66@gmail.com>
2018-07-31 18:40:53 +0200

    bug fix

--- src/main/java/it/polito/ai/lab03/repository/model/PositionRepresentationCoordinates.java
+++ src/main/java/it/polito/ai/lab03/repository/model/PositionRepresentationCoordinates.java
@@ -1,14 +1,9 @@
 package it.polito.ai.lab03.repository.model;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
 import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Objects;
-
-@Document(collection = "positionsReprCoord")
 public class PositionRepresentationCoordinates {
 
     private String userId;
--- src/main/java/it/polito/ai/lab03/repository/model/PositionRepresentationTimestamp.java
+++ src/main/java/it/polito/ai/lab03/repository/model/PositionRepresentationTimestamp.java
@@ -1,11 +1,6 @@
 package it.polito.ai.lab03.repository.model;
 
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.Objects;
-
-@Document(collection = "positionsReprTime")
-public class PositionRepresentationTimestamp extends Position {
+public class PositionRepresentationTimestamp {
 
     private String userId;
     private String archiveId;
@@ -17,32 +12,26 @@ public class PositionRepresentationTimestamp extends Position {
         this.timestamp = Math.round( position.getTimestamp() / 100 ) * 100;
     }
 
-    @Override
     public String getUserId() {
         return userId;
     }
 
-    @Override
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
-    @Override
     public String getArchiveId() {
         return archiveId;
     }
 
-    @Override
     public void setArchiveId(String archiveId) {
         this.archiveId = archiveId;
     }
 
-    @Override
     public long getTimestamp() {
         return timestamp;
     }
 
-    @Override
     public void setTimestamp(long timestamp) {
         this.timestamp = timestamp;
     }
